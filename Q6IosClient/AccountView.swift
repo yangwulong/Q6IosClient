aeee4d6715b2f909517e621e68ed2fa32ff6a059
yang wulong <yangwulong@macbook-pro.local>
2016-05-13 11:43:14 +1000

    111

---
+++ Q6IosClient/AccountView.swift
@@ -0,0 +1,46 @@
+//
+//  AccountView.swift
+//  Q6IosClient
+//
+//  Created by yang wulong on 12/05/2016.
+//  Copyright © 2016 q6. All rights reserved.
+//
+
+import Foundation
+
+public class AccountView {
+    
+    var AccountID = String()
+    var AccountName = String()
+    var AccountNameWithAccountNo = String()
+    var AccountNumber = Int()
+    var AccountNumberPrifix = String()
+    var AccountType = String()
+    var BankAccountName = String()
+    var BankAccountNumber = String()
+    var BankCode = String()
+    var BSBNumber = String()
+    var CompanyTradingName = String()
+    var CurrentBalance = Double()
+    var Description = String()
+    var DirectEntryUserID = String()
+    var IsAccountLinked = Bool()
+    var IsBank = Bool()
+    var IsBuiltIn = Bool()
+    var IsBuy = Bool()
+    var IsCreateABAFile = Bool()
+    var IsDeposit = Bool()
+    var IsInactive = Bool()
+    var IsIncludeOpeningBalance = Bool()
+    var IsInventory = Bool()
+    var IsPurchase = Bool()
+    var IsSale = Bool()
+    var IsSell = Bool()
+    var OpeningBalance = Double()
+    var OpeningBalanceDate = NSDate?()
+    var OpeningBalanceType = String()
+    var SortNo = Int()
+    var TaxCodeID = String?()
+    var TaxCodeName = String()
+    var TaxRate = Double()
+}
---
+++ Q6IosClient/TaxCodeView.swift
@@ -0,0 +1,29 @@
+//
+//  TaxCodeView.swift
+//  Q6IosClient
+//
+//  Created by yang wulong on 13/05/2016.
+//  Copyright © 2016 q6. All rights reserved.
+//
+
+import Foundation
+
+public class TaxCodeView {
+    
+    var TaxCodeID = String()
+    var TaxCodeName = String()
+    var TaxCodeType = String()
+    var TaxDescription = String()
+    var TaxRate = String()
+    var BASItemCode = String()
+    var IsBuiltIn = Bool()
+    var IsCollectTax = Bool()
+    var IsDeleted = Bool()
+    var IsInactive = Bool()
+    var LinkedAccountID = String()
+    var LinkedAccountNameWithAccountNo = String()
+    var LinkedWithAccounts = Bool()
+    var Purpose = String()
+    var SortNo = Int()
+    
+}

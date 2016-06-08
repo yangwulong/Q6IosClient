94f097de038397c092ab2fbae63501fa1c3ac52a
yang wulong <yangwulong@macbook-pro.local>
2016-06-08 14:39:13 +1000

    ss

---
+++ Q6IosClient/SalesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SalesViewController.swift
+//  Q6IosClient
+//
+//  Created by yang wulong on 8/06/2016.
+//  Copyright © 2016 q6. All rights reserved.
+//
+
+import UIKit
+
+class SalesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
